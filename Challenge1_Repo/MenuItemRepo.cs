3233160d7576d243f54f5cf9da6fb897c204421f
Maggie <88403063+maggiesnader@users.noreply.github.com>
2021-09-28 12:34:14 -0400

    challenge1 poco and repo added

---
+++ Challenge1_Repo/MenuItemRepo.cs
@@ -0,0 +1,78 @@
+﻿using Challenge1_POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1_Repo
+{
+    public class MenuItemRepo
+    {
+        private List<MenuItem> _ListOfMenuItems = new List<MenuItem>();
+
+        //CREATE - ADD
+        public bool AddMenuItemToList(MenuItem itemToBeAdded)
+        {
+            if(itemToBeAdded != null)
+            {
+                _ListOfMenuItems.Add(itemToBeAdded);
+                return true;
+            }
+            return false;
+        }
+
+        //READ - VIEW
+        public List<MenuItem> ViewAllThread()
+        {
+            return _ListOfMenuItems;
+        }
+
+        public MenuItem ViewItemByNumber(int mealNumber)
+        {
+            foreach(MenuItem meal in _ListOfMenuItems)
+            {
+                if(meal.MealNumber == mealNumber)
+                {
+                    return meal;
+                }
+            }
+            return null;
+        }
+
+        //UPDATE--NOT REQUIRED ON PROMPT
+        public bool UpdateExistingMenuItem(int mealNumber, MenuItem newMenuItemInfo)
+        {
+            //find the content
+            MenuItem oldMenuItemInfo = ViewItemByNumber(mealNumber);
+            //update the content
+            if(oldMenuItemInfo != null)
+            {
+                oldMenuItemInfo.MealNumber = newMenuItemInfo.MealNumber;
+                oldMenuItemInfo.MealName = newMenuItemInfo.MealName;
+                oldMenuItemInfo.MealDescription = newMenuItemInfo.MealDescription;
+                oldMenuItemInfo.MealIngredients = newMenuItemInfo.MealIngredients;
+                oldMenuItemInfo.MealPrice = newMenuItemInfo.MealPrice;
+                return true;
+            }
+            return false;
+        }
+
+        //DELETE
+        public bool RemoveMenuItem (int mealNumber)
+        {
+            MenuItem meal = ViewItemByNumber(mealNumber);
+            if(meal == null)
+            {
+                return false;
+            }
+            int initialCount = _ListOfMenuItems.Count;
+            _ListOfMenuItems.Remove(meal);
+            if(initialCount > _ListOfMenuItems.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
