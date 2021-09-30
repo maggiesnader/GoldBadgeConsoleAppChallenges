6c662a4381968bb8f59896f4631051ee9f6322cb
Maggie <88403063+maggiesnader@users.noreply.github.com>
2021-09-30 17:18:51 -0400

    challenge4 pocotests complete

--- Challenge4_Tests/OutingPOCOTests.cs
+++ Challenge4_Tests/OutingPOCOTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Challenge4_POCO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Challenge4_Tests
@@ -7,8 +8,66 @@ namespace Challenge4_Tests
     public class OutingPOCOTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SetOutingTitle_ShouldSetCorrectString()
         {
+            Outing outing = new Outing();
+            outing.OutingTitle = "Winter Holiday 2020";
+            string expected = "Winter Holiday 2020";
+            string actual = outing.OutingTitle;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SetOutingID_ShouldSetCorrectInt()
+        {
+            Outing outing = new Outing();
+            outing.ID = 1;
+            int expected = 1;
+            int actual = outing.ID;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SetOutingType_ShouldSetCorrectOutingType()
+        {
+            Outing outing = new Outing();
+            outing.TypeOfOuting = OutingType.Golf;
+            OutingType expected = OutingType.Golf;
+            OutingType actual = outing.TypeOfOuting;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SetDateOfOuting_ShouldSetCorrectDateTime()
+        {
+            Outing outing = new Outing();
+            outing.OutingDate = new DateTime(2020, 12, 18);
+            DateTime expected = new DateTime(2020, 12, 18);
+            DateTime actual = outing.OutingDate;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SetNumberOfAttendees_ShouldSetCorrectInt()
+        {
+            Outing outing = new Outing();
+            outing.NumberOfAttendees = 25;
+            int expected = 25;
+            int actual = outing.NumberOfAttendees;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SetCostPerPerson_ShouldSetCorrectDecimal()
+        {
+            Outing outing = new Outing();
+            outing.CostPerPerson = 59.23m;
+            decimal expected = 59.23m;
+            decimal actual = outing.CostPerPerson;
+            Assert.AreEqual(expected, actual);
+        }
+        public void SetTotalCost_ShouldSetCorrectDecimal()
+        {
+            Outing outing = new Outing();
+            outing.TotalCost = 1480.75m;
+            decimal expected = 1480.75m;
+            decimal actual = outing.TotalCost;
+            Assert.AreEqual(expected, actual);
         }
     }
 }
